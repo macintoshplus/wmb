e59fbea59f13dc2fda2610cfdbf48dfc61eac56b
macintoshplus <jean-baptiste@nahan.fr>
2015-02-13 17:12:35 +0100

    -

--- Visitor/WorkflowVisitorNodeCollector.php
+++ Visitor/WorkflowVisitorNodeCollector.php
@@ -67,10 +67,10 @@ class WorkflowVisitorNodeCollector extends WorkflowVisitor
      *
      * @param Workflow $workflow
      */
-    public function __construct( Workflow $workflow )
+    public function __construct(Workflow $workflow)
     {
         parent::__construct();
-        $workflow->accept( $this );
+        $workflow->accept($this);
     }
 
     /**
@@ -78,34 +78,26 @@ class WorkflowVisitorNodeCollector extends WorkflowVisitor
      *
      * @param WorkflowVisitableInterface $visitable
      */
-    protected function doVisit( WorkflowVisitableInterface $visitable )
+    protected function doVisit(WorkflowVisitableInterface $visitable)
     {
-        if ( $visitable instanceof Workflow )
-        {
-            $visitable->startNode->setId( ++$this->nextId );
+        if ($visitable instanceof Workflow) {
+            $visitable->startNode->setId(++$this->nextId);
             $this->startNode = $visitable->startNode;
 
-            $visitable->endNode->setId( ++$this->nextId );
+            $visitable->endNode->setId(++$this->nextId);
             $this->endNode = $visitable->endNode;
 
-            if ( count( $visitable->finallyNode->getOutNodes() ) > 0 )
-            {
+            if (count($visitable->finallyNode->getOutNodes()) > 0) {
                 $this->finallyNode = $visitable->finallyNode;
-                $visitable->finallyNode->setId( ++$this->nextId );
+                $visitable->finallyNode->setId(++$this->nextId);
             }
-        }
-
-        else if ( $visitable instanceof WorkflowNode )
-        {
-            if ( $visitable !== $this->startNode &&
+        } else if ($visitable instanceof WorkflowNode) {
+            if ($visitable !== $this->startNode &&
                  $visitable !== $this->endNode &&
-                 $visitable !== $this->finallyNode )
-            {
+                 $visitable !== $this->finallyNode) {
                 $id = ++$this->nextId;
-                $visitable->setId( $id );
-            }
-            else
-            {
+                $visitable->setId($id);
+            } else {
                 $id = $visitable->getId();
             }
 
@@ -120,19 +112,16 @@ class WorkflowVisitorNodeCollector extends WorkflowVisitor
      */
     public function getNodes()
     {
-        if ( $this->finallyNode !== null && !$this->finallyNodeVisited )
-        {
-            $this->finallyNode->accept( $this );
+        if ($this->finallyNode !== null && !$this->finallyNodeVisited) {
+            $this->finallyNode->accept($this);
             $this->finallyNode = true;
         }
 
-        if ( !$this->sorted )
-        {
-            ksort( $this->nodes );
+        if (!$this->sorted) {
+            ksort($this->nodes);
             $this->sorted = true;
         }
 
         return $this->nodes;
     }
 }
-
