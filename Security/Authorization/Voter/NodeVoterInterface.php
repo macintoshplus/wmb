16db0d83387f44f46cb3bb6986070a329a446bf3
macintoshplus <jean-baptiste@nahan.fr>
2015-02-16 07:20:08 +0100

    -

---
+++ Security/Authorization/Voter/NodeVoterInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace JbNahan\Bundle\WorkflowManagerBundle\Security\Authorization\Voter;
+
+interface NodeVoterInterface
+{
+    /**
+     * @return boolean
+     */
+    public function getRoles();
+
+    /**
+     * @param string $username
+     * @return booblean
+     */
+    public function hasRoleUsername($username);
+}
