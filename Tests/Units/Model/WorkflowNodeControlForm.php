76c10b8052cb4ce2d799c42b8900e0590763e246
macintoshplus <jean-baptiste@nahan.fr>
2015-02-20 16:36:18 +0100

    -

--- Tests/Units/Model/WorkflowNodeControlForm.php
+++ Tests/Units/Model/WorkflowNodeControlForm.php
@@ -9,114 +9,114 @@ use Mock;
 
 class WorkflowNodeControlForm extends Units\Test
 {
-	public function test_execute()
-	{
-		$controller = new \atoum\mock\controller();
-		$controller->__construct = function() {};
+    public function test_execute()
+    {
+        $controller = new \atoum\mock\controller();
+        $controller->__construct = function () {};
 
-		$entityManager = new Mock\Doctrine\ORM\EntityManagerInterface();
-		//$meta = new Mock\Doctrine\ORM\Mapping\ClassMetadata();
-		//$repo = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Repository\ExecutionRepository(null, $meta);
-		//$repo->getMockController()->getExecutionById = array();
-		//$entityManager->getMockController()->getRepository = $repo;
+        $entityManager = new Mock\Doctrine\ORM\EntityManagerInterface();
+        //$meta = new Mock\Doctrine\ORM\Mapping\ClassMetadata();
+        //$repo = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Repository\ExecutionRepository(null, $meta);
+        //$repo->getMockController()->getExecutionById = array();
+        //$entityManager->getMockController()->getRepository = $repo;
 
-		$definitionService = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowDefinitionStorageInterface();
+        $definitionService = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowDefinitionStorageInterface();
 
-		$security = new Mock\Symfony\Component\Security\Core\SecurityContextInterface();
+        $security = new Mock\Symfony\Component\Security\Core\SecurityContextInterface();
 
-		$mockExecute = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowDatabaseExecution($entityManager, $definitionService, $security, null, $controller);
-		$mockExecute->getMockController()->getId = 1;
+        $mockExecute = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowDatabaseExecution($entityManager, $definitionService, $security, null, $controller);
+        $mockExecute->getMockController()->getId = 1;
 
-		$node = new \JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowNodeControlForm(array('internal_name'=>'form_1','out_date'=>new \DateTime('2015-02-20')));
-		
-
-		$trueNode  = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowNodeAction( 'PrintTrue' );
-		$falseNode = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowNodeAction( 'PrintFalse' );
-
-		$node->addSelectOutNode($trueNode, $falseNode);
-
-		$node->activate($mockExecute);
-
-		$this->assert->boolean($node->execute($mockExecute))->isFalse();
-
-		$mockExecute->setVariable('form_1',array(array('data'=>'toto')));
-
-		$this->assert->boolean($node->execute($mockExecute))->isFalse();
-
-		$this->assert->array($mockExecute->getActivatedNodes())->hasSize(1);
-		//var_dump($mockExecute->getActivatedNodes());
-
-	}
+        $node = new \JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowNodeControlForm(array('internal_name'=>'form_1','out_date'=>new \DateTime('2099-02-20')));
 
 
-	public function test_execute_date_pass_no_answers()
-	{
-		$controller = new \atoum\mock\controller();
-		$controller->__construct = function() {};
+        $trueNode  = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowNodeAction( 'PrintTrue' );
+        $falseNode = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowNodeAction( 'PrintFalse' );
 
-		$entityManager = new Mock\Doctrine\ORM\EntityManagerInterface();
-		//$meta = new Mock\Doctrine\ORM\Mapping\ClassMetadata();
-		//$repo = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Repository\ExecutionRepository(null, $meta);
-		//$repo->getMockController()->getExecutionById = array();
-		//$entityManager->getMockController()->getRepository = $repo;
+        $node->addSelectOutNode($trueNode, $falseNode);
 
-		$definitionService = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowDefinitionStorageInterface();
+        $node->activate($mockExecute);
 
-		$security = new Mock\Symfony\Component\Security\Core\SecurityContextInterface();
+        $this->assert->boolean($node->execute($mockExecute))->isFalse();
 
-		$mockExecute = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowDatabaseExecution($entityManager, $definitionService, $security, null, $controller);
-		$mockExecute->getMockController()->getId = 1;
+        $mockExecute->setVariable('form_1',array(array('data'=>'toto')));
 
-		$node = new \JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowNodeControlForm(array('internal_name'=>'form_1','out_date'=>new \DateTime('2015-02-01')));
-		
+        $this->assert->boolean($node->execute($mockExecute))->isFalse();
 
-		$continueNode  = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowNodeAction( 'PrintContinue' );
-		$elseNode = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowNodeAction( 'PrintElse' );
+        $this->assert->array($mockExecute->getActivatedNodes())->hasSize(1);
+        //var_dump($mockExecute->getActivatedNodes());
 
-		$node->addSelectOutNode($continueNode, $elseNode);
+    }
 
-		$node->activate($mockExecute);
 
-		$this->assert->boolean($node->execute($mockExecute))->isTrue();
-		//var_dump($mockExecute->getActivatedNodes());
+    public function test_execute_date_pass_no_answers()
+    {
+        $controller = new \atoum\mock\controller();
+        $controller->__construct = function () {};
 
-		$this->assert->array($mockExecute->getActivatedNodes())->hasSize(2);
+        $entityManager = new Mock\Doctrine\ORM\EntityManagerInterface();
+        //$meta = new Mock\Doctrine\ORM\Mapping\ClassMetadata();
+        //$repo = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Repository\ExecutionRepository(null, $meta);
+        //$repo->getMockController()->getExecutionById = array();
+        //$entityManager->getMockController()->getRepository = $repo;
 
-	}
+        $definitionService = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowDefinitionStorageInterface();
 
-	public function test_execute_date_pass_with_answers()
-	{
-		$controller = new \atoum\mock\controller();
-		$controller->__construct = function() {};
+        $security = new Mock\Symfony\Component\Security\Core\SecurityContextInterface();
 
-		$entityManager = new Mock\Doctrine\ORM\EntityManagerInterface();
-		//$meta = new Mock\Doctrine\ORM\Mapping\ClassMetadata();
-		//$repo = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Repository\ExecutionRepository(null, $meta);
-		//$repo->getMockController()->getExecutionById = array();
-		//$entityManager->getMockController()->getRepository = $repo;
+        $mockExecute = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowDatabaseExecution($entityManager, $definitionService, $security, null, $controller);
+        $mockExecute->getMockController()->getId = 1;
 
-		$definitionService = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowDefinitionStorageInterface();
+        $node = new \JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowNodeControlForm(array('internal_name'=>'form_1','out_date'=>new \DateTime('2015-02-01')));
 
-		$security = new Mock\Symfony\Component\Security\Core\SecurityContextInterface();
 
-		$mockExecute = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowDatabaseExecution($entityManager, $definitionService, $security, null, $controller);
-		$mockExecute->getMockController()->getId = 1;
+        $continueNode  = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowNodeAction( 'PrintContinue' );
+        $elseNode = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowNodeAction( 'PrintElse' );
 
-		$node = new \JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowNodeControlForm(array('internal_name'=>'form_1','out_date'=>new \DateTime('2015-02-01')));
-		
+        $node->addSelectOutNode($continueNode, $elseNode);
 
-		$continueNode  = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowNodeAction( 'PrintContinue' );
-		$elseNode = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowNodeAction( 'PrintElse' );
+        $node->activate($mockExecute);
 
-		$node->addSelectOutNode($continueNode, $elseNode);
+        $this->assert->boolean($node->execute($mockExecute))->isTrue();
+        //var_dump($mockExecute->getActivatedNodes());
 
-		$node->activate($mockExecute);
+        $this->assert->array($mockExecute->getActivatedNodes())->hasSize(2);
 
-		$mockExecute->setVariable('form_1', array(array('data'=>'toto')));
+    }
 
-		$this->assert->boolean($node->execute($mockExecute))->isTrue();
-		//var_dump($mockExecute->getActivatedNodes());
-		$this->assert->array($mockExecute->getActivatedNodes())->hasSize(2);
+    public function test_execute_date_pass_with_answers()
+    {
+        $controller = new \atoum\mock\controller();
+        $controller->__construct = function () {};
 
-	}
+        $entityManager = new Mock\Doctrine\ORM\EntityManagerInterface();
+        //$meta = new Mock\Doctrine\ORM\Mapping\ClassMetadata();
+        //$repo = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Repository\ExecutionRepository(null, $meta);
+        //$repo->getMockController()->getExecutionById = array();
+        //$entityManager->getMockController()->getRepository = $repo;
+
+        $definitionService = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowDefinitionStorageInterface();
+
+        $security = new Mock\Symfony\Component\Security\Core\SecurityContextInterface();
+
+        $mockExecute = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowDatabaseExecution($entityManager, $definitionService, $security, null, $controller);
+        $mockExecute->getMockController()->getId = 1;
+
+        $node = new \JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowNodeControlForm(array('internal_name'=>'form_1','out_date'=>new \DateTime('2015-02-01')));
+
+
+        $continueNode  = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowNodeAction( 'PrintContinue' );
+        $elseNode = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowNodeAction( 'PrintElse' );
+
+        $node->addSelectOutNode($continueNode, $elseNode);
+
+        $node->activate($mockExecute);
+
+        $mockExecute->setVariable('form_1', array(array('data'=>'toto')));
+
+        $this->assert->boolean($node->execute($mockExecute))->isTrue();
+        //var_dump($mockExecute->getActivatedNodes());
+        $this->assert->array($mockExecute->getActivatedNodes())->hasSize(2);
+
+    }
 }
