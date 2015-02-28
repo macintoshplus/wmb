5a0696653f6974339e7eb85607c95735f6026afc
macintoshplus <jean-baptiste@nahan.fr>
2015-02-28 07:17:30 +0100

    -

--- Tests/Units/Model/WorkflowNodeEmail.php
+++ Tests/Units/Model/WorkflowNodeEmail.php
@@ -12,7 +12,7 @@ class WorkflowNodeEmail extends Units\Test
     public function test_send_ok()
     {
         $controller = new \atoum\mock\controller();
-        $controller->__construct = function() {};
+        $controller->__construct = function () {};
 
         $entityManager = new Mock\Doctrine\ORM\EntityManagerInterface();
         //$meta = new Mock\Doctrine\ORM\Mapping\ClassMetadata();
@@ -57,7 +57,7 @@ class WorkflowNodeEmail extends Units\Test
     public function test_send_ko()
     {
         $controller = new \atoum\mock\controller();
-        $controller->__construct = function() {};
+        $controller->__construct = function () {};
 
         $entityManager = new Mock\Doctrine\ORM\EntityManagerInterface();
         //$meta = new Mock\Doctrine\ORM\Mapping\ClassMetadata();
@@ -72,7 +72,7 @@ class WorkflowNodeEmail extends Units\Test
         $controllerSwift = new \atoum\mock\controller();
         $controllerSwift->__construct = function () {};
         $mockSwift = new Mock\Swift_Mailer(new Mock\Swift_Transport(), $controllerSwift);
-        // $mockSwift->getMockController()->send = function($arg1, &$arg2){
+        // $mockSwift->getMockController()->send = function ($arg1, &$arg2) {
         //     $arg2[]='toto@toto.fr';
         //     return 0;
         // };
@@ -82,11 +82,11 @@ class WorkflowNodeEmail extends Units\Test
 
         $mockExecute = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowDatabaseExecution($entityManager, $definitionService, $security, $mockLogger, $mockSwift, $mockTwig, null, $controller);
         $mockExecute->getMockController()->getId = 1;
-        $mockExecute->getMockController()->mailerSend = function ($arg1, &$arg2){
+        $mockExecute->getMockController()->mailerSend = function ($arg1, &$arg2) {
             $arg2[]='toto@toto.fr';
             return 0;
         };
-        
+
         $mockDefinition = new Mock\JbNahan\Bundle\WorkflowManagerBundle\Model\Workflow('test');
         $mockExecute->workflow = $mockDefinition;
 
@@ -103,6 +103,16 @@ class WorkflowNodeEmail extends Units\Test
         $this->mock($mockLogger)->call('info')->once();
         $this->mock($mockLogger)->call('warning')->once();
         $this->mock($mockLogger)->call('error')->never();
+
+    }
+
+    public function test_getter_setter()
+    {
+        $node = new \JbNahan\Bundle\WorkflowManagerBundle\Model\WorkflowNodeEmail();
+        $this->assert->string($node->getFrom())->isEqualTo('exemple@toto.fr');
+        $this->assert->array($node->getTo())->hasSize(1)->contains('exemple@toto.fr');
+        $this->assert->string($node->getSubject())->isEqualTo('Email from Workflow');
+        $this->assert->string($node->getBody())->contains('This is a email send by Workflow');
 
     }
 }
